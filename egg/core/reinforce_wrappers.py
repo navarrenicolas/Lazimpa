94fc02213b0709549b3b85117d1fdcf01ccd859b
navarrenicolas <navarrenicolas@gmail.com>
2022-04-22 13:42:23 +0200

    no comment

--- egg/core/reinforce_wrappers.py
+++ egg/core/reinforce_wrappers.py
@@ -870,7 +870,7 @@ class SenderReceiverRnnReinforceNoisy(nn.Module):
     def forward(self, sender_input, labels, receiver_input=None):
         message, log_prob_s, entropy_s = self.sender(sender_input)
         message = add_noise(message)
-        message_length = find_lengths(message)
+        message_lengths = find_lengths(message)
 
         receiver_output, log_prob_r, entropy_r = self.receiver(message, receiver_input, message_lengths)
 
