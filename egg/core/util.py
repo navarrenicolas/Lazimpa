097ce945fcc0384ac001f913507371e4a46a9611
Matthew Loder <i.loder.matthew@gmail.com>
2022-04-28 12:51:16 +0200

    Fixed bug in add_noise

--- egg/core/util.py
+++ egg/core/util.py
@@ -503,7 +503,7 @@ def add_noise(messages: torch.Tensor,vocab_size,rand,threshold,neighbors=None)->
         for message in messages:
             for idx, char in enumerate(message):
                 if random.randint(0,99)/100.0 < threshold:
-                    c = message[idx].cpu()
+                    c = char.item()
                     message[idx] = random.choice(neighbors[c])
     return messages
 
